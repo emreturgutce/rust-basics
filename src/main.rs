18bd5bd8cf22f2a66fc85850feecea31734d9863
Emre Turgut <emre.turgut@trendyol.com>
2022-05-02 19:26:30 +0300

    image combiner

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,67 @@
-use std::env::{args, Args};
+mod args;
+use args::Args;
+use image::{io::Reader, DynamicImage, ImageFormat, imageops::FilterType::Triangle, GenericImageView};
+use std::{io::BufReader, fs::File};
 
-fn main() {
-    let mut args: Args = args();
-    let first = args.nth(1).unwrap();
-    let operator = args.nth(0).unwrap().chars().next().unwrap();
-    let second = args.nth(0).unwrap();
-
-    let first_number = first.parse::<f32>().unwrap();
-    let second_number = second.parse::<f32>().unwrap();
-    let result = operate(operator, first_number, second_number);
-
-    println!(
-        "{:?}",
-        output(first_number, operator, second_number, result)
-    );
+#[derive(Debug)]
+enum ImageDataErrors {
+    DifferentImageFormats,
 }
 
-fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
-    match operator {
-        '+' => first_number + second_number,
-        '-' => first_number - second_number,
-        '/' => first_number / second_number,
-        '*' | 'x'  | 'X' => first_number * second_number,
-        _ => panic!("Invalid operator used"),
+struct FloatingImage {
+    width: u32,
+    height: u32,
+    data: Vec<u8>,
+    name: String,
+}
+
+impl FloatingImage {
+    fn new(width: u32, height: u32, name: String) -> Self {
+        let buffer_capacity = height * width * 4;
+        let buffer = Vec::with_capacity(buffer_capacity.try_into().unwrap());
+        FloatingImage {
+            width,
+            height,
+            data,
+            name,
+        }
     }
 }
 
-fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
-    format!(
-        "{} {} {} = {}",
-        first_number, operator, second_number, result
-    )
+fn main() -> Result<(), ImageDataErrors> {
+    let args = Args::new();
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
+    
+    if image_format_1 != image_format_2 {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+
+    let (image_1, image_2) = standardise_size(image_1, image_2);
+
+    Ok(())
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap();
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image = image_reader.decode().unwrap();
+    (image, image_format)
+}
+
+fn get_smallest_dimension(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+    return if pix_1 < pix_2 { dim_1 } else { dim_2 }
+}
+
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_smallest_dimension(image_1.dimensions(), image_2.dimensions());
+    println!("width: {}, height: {}", width, height);
+
+    if image_2.dimensions() == (width, height) {
+        (image_1.resize_exact(width, height, Triangle), image_2)
+    } else {
+        (image_1, image_2.resize_exact(width, height, Triangle))
+    }
 }
